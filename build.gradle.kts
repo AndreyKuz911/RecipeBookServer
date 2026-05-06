56ad052c5c57c3629a8052f098c725891af3a871
Xenia2005 <andreyminkov2018@gmail.com>
2026-05-06 19:10:26 +0300

    Add Gradle wrapper and root build files

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    application
+    kotlin("jvm") version "2.2.10"
+    kotlin("plugin.serialization") version "2.2.10"
+}
+
+group = "com.recipebook"
+version = "1.0.0"
+
+val ktorVersion = "3.1.3"
+
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
+    }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-config-yaml:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-cors-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-default-headers-jvm:$ktorVersion")
+
+    implementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.61.0")
+    implementation("org.postgresql:postgresql:42.7.7")
+    implementation("com.h2database:h2:2.3.232")
+    implementation("com.zaxxer:HikariCP:6.3.0")
+    implementation("io.github.cdimascio:dotenv-kotlin:6.5.1")
+    implementation("org.mindrot:jbcrypt:0.4")
+    implementation("ch.qos.logback:logback-classic:1.5.18")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+
+    testImplementation(kotlin("test"))
+    testImplementation("io.ktor:ktor-server-test-host-jvm:$ktorVersion")
+    testImplementation("com.h2database:h2:2.3.232")
+    testImplementation("org.jetbrains.exposed:exposed-core:0.61.0")
+    testImplementation("org.jetbrains.exposed:exposed-jdbc:0.61.0")
+    testImplementation("org.jetbrains.exposed:exposed-java-time:0.61.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
