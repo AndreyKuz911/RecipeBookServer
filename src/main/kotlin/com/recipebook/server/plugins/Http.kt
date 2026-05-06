b0370534bb3741ef5620ea2a4fa9a06cd1cda89e
Xenia2005 <andreyminkov2018@gmail.com>
2026-05-06 19:10:26 +0300

    Add HTTP and monitoring plugins

---
+++ src/main/kotlin/com/recipebook/server/plugins/Http.kt
@@ -0,0 +1,21 @@
+package com.recipebook.server.plugins
+
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.defaultheaders.DefaultHeaders
+
+fun Application.configureHttp() {
+    install(DefaultHeaders)
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.Authorization)
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Delete)
+    }
+}
